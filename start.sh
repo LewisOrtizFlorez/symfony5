87dbe2996ba5dd783b389a0cdb85f548546e2af2
lewis Ortiz <lewosdortizf01@gmail.com>
2022-01-31 11:35:56 -0500

    sh 7

--- start.sh
+++ start.sh
@@ -2,7 +2,5 @@
 wget https://get.symfony.com/cli/installer -O - | bash
 mv /root/.symfony/bin/symfony /usr/local/bin/symfony
 cd /var/www/app/
-ls
-symfony
-symfony install
+symfony composer install
 php-fpm
